f818495848a929b8a7ecb42ed570ed0f6e4e1b2d
Ajay Rawat <ajayrawat0706@gmail.com>
2023-09-22 21:37:39 +0530

    fix6

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express=require('express');
-const morgan =require('morgan');
+// const morgan =require('morgan');
 const mongoose = require('mongoose');
 const itemRouter =require('./routes/item');
 const userRouter=require('./routes/user');
@@ -9,22 +9,26 @@ const orderRouter=require('./routes/orders');
 const cors = require('cors');
 const server=express();
 
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
-console.log('database connected');
-}
+server.use(express.static(path.resolve(__dirname,'build')))
 server.use(cors());
 server.use(express.json());
-server.use(morgan('default'))
-server.use(express.static(process.env.PUBLIC_DIR))
+// server.use(morgan('default'))
+
 server.use('/items',itemRouter.router);
 server.use('/user',userRouter.router);
 server.use('/orders',orderRouter.router);
 server.use('*',(req,res)=>{
-  res.sendFile(path.resolve('build','index.html'))
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
 })
-server.listen(process.env.PORT, () => {
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/foodApp');
+console.log('database connected');
+}
+
+server.listen(8010,()=>{
   console.log('server started');
-});
+})
